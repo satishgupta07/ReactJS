6e96744f091821bed59808026a40d7f26779d1fe
Satish Kumar <satish.kumar06@nagarro.com>
2022-09-24 14:57:43 +0530

    Access Props & Destructuring

--- contact-app/src/components/ContactList.js
+++ contact-app/src/components/ContactList.js
@@ -1,22 +1,14 @@
 import React from 'react'
+import { ContactCard } from './ContactCard';
 
 const ContactList = (props) => {
     console.log(props);
 
     const renderContactList = props.contacts.map((contact) => {
-        return (
-            <div className='item'>
-                <div className='content'>
-                    <div className='header'>{contact.name}</div>
-                    <div>{contact.email}</div>
-                </div>
-                <i className='trash alternate outline icon'></i>
-            </div>
-        )
+        return (<ContactCard contact={contact}/>)
     })
-  return (
-    <div className='ui celled list'>{renderContactList}</div>
-  )
+    
+    return <div className="ui celled list">{renderContactList}</div>
 }
 
 export default ContactList
