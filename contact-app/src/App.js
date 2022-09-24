9eea5ec925efd54beffed643915bf5cdc142c719
Satish Kumar <satish.kumar06@nagarro.com>
2022-09-24 13:20:18 +0530

    Rendering List & Props

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import AddContact from './components/AddContact'
+import ContactList from './components/ContactList'
 import Header from './components/Header'
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Satish",
+      email: "satish@gmail.com"
+    },
+    {
+      id: "2",
+      name: "Dipesh",
+      email: "malvia@gmail.com"
+    }
+  ]
   return (
     <div className="ui container">
       <Header />
       <AddContact />
+      <ContactList contacts={contacts} />
     </div>
   )
 }
